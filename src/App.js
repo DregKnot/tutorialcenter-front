86a391ecd4f75f58c84adc36d0dff9e36e77249c
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2026-02-09 16:34:35 +0100

    Removing login route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import About from "./pages/public/About";
 import Home from "./pages/public/Home.jsx";
-import Login from "./pages/public/Login.jsx";
+// import Login from "./pages/public/Login.jsx";
 import SignUp from "./pages/public/SignUp.jsx";
 import { Route, Routes } from "react-router-dom";
 import StudentRegistration from "./pages/public/StudentSignUp/StudentRegistration.jsx";
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/register" element={<SignUp />} />
 
