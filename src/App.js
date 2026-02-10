123fdd93db4c233f6e72589e9cbe2e34a9960fd7
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2026-02-10 18:28:08 +0100

    Adding some dummy pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import { StudentTrainingDuration } from "./pages/public/StudentSignUp/StudentTra
 import { StudentTrainingPayment } from "./pages/public/StudentSignUp/StudentTrainingPayment.jsx";
 import StudentLogin from "./pages/public/StudentLogin.jsx";
 import StudentDashboard from "./pages/students/StudentDashboard.jsx";
+import ComingSoon from "./pages/public/ComingSoon.jsx";
+import Unauthorized from "./pages/public/Unauthorized.jsx";
+import NotFound from "./pages/public/NotFound.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 
 function App() {
@@ -20,14 +24,17 @@ function App() {
     <>
       <Routes>
         {/* Public Routes */}
+        <Route path="*" element={<NotFound/>} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/register" element={<SignUp />} />
+        <Route path="/comingsoon" element={<ComingSoon/>} />
+        <Route path="/unauthorized" element={<Unauthorized/>} />
 
         {/* Student Public Registration Routes */}
         <Route path="/student/login" element={<StudentLogin/>} />
-        <Route path="/student/dashboard" element={<StudentDashboard/> }/>
+        {/* <Route path="/student/dashboard" element={<StudentDashboard/> }/> */}
         <Route path="/register/student" element={<StudentRegistration />} />
         <Route path="/register/student/biodata" element={<StudentBiodata/>} />
         <Route path="/register/student/phone/verify" element={<StudentPhoneVerification />} />
@@ -36,6 +43,11 @@ function App() {
         <Route path = "/register/student/subject/selection" element={<StudentSubjectSelection />} />
         <Route path = "/register/student/training/duration" element={<StudentTrainingDuration />} />
         <Route path = "/register/student/training/payment" element={<StudentTrainingPayment />} />
+
+        {/* Student Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+        <Route path="/student/dashboard" element={<StudentDashboard/> }/>
+        </Route>
       </Routes>
     </>
   );
